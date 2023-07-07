5055f56b7a5d76b2d55ae9a8b67abff11266f4ed
Manuel Tadeo Guerrero <mguerrero@roshka.com>
2023-07-07 11:21:58 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Amazon());
+}
+
+class Amazon extends StatelessWidget {
+  const Amazon({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
